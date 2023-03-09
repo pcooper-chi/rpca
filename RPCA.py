934f85dc1fdde63df68dee00ff3e5f225ba51d1f
Phil Cooper <37032821+pcooper-chi@users.noreply.github.com>
2023-03-08 18:29:27 -0600

    Add files via upload

--- RPCA.py
+++ RPCA.py
@@ -1,6 +1,7 @@
 import numpy as np
+from scipy.sparse.linalg import svds
 
-def RPCA(X):
+def RPCA(X, n_iter=1000):
     
     def shrink(X,tau):
         Y = np.abs(X)-tau
@@ -20,7 +21,35 @@ def RPCA(X):
     Y = np.zeros_like(X)
     L = np.zeros_like(X)
     count = 0
-    while (np.linalg.norm(X-L-S) > thresh) and (count < 1000):
+    while (np.linalg.norm(X-L-S) > thresh) and (count < n_iter):
+        L = SVT(X-S+(1/mu)*Y,1/mu)
+        S = shrink(X-L+(1/mu)*Y,lambd/mu)
+        Y = Y + mu*(X-L-S)
+        count += 1
+        
+    return L,S
+
+def RPCA_partial(X, rank=10, n_iter=1000):
+    
+    def shrink(X,tau):
+        Y = np.abs(X)-tau
+        return np.sign(X) * np.maximum(Y,np.zeros_like(Y))
+    
+    def SVT(X,tau):
+        U,S,VT = svds(X.astype(float),k=rank)
+        out = U @ np.diag(shrink(S,tau)) @ VT
+        return out
+    
+    n1,n2 = X.shape
+    mu = n1*n2/(4*np.sum(np.abs(X.reshape(-1))))
+    lambd = 1/np.sqrt(np.maximum(n1,n2))
+    thresh = 10**(-7) * np.linalg.norm(X)
+    
+    S = np.zeros_like(X)
+    Y = np.zeros_like(X)
+    L = np.zeros_like(X)
+    count = 0
+    while (np.linalg.norm(X-L-S) > thresh) and (count < n_iter):
         L = SVT(X-S+(1/mu)*Y,1/mu)
         S = shrink(X-L+(1/mu)*Y,lambd/mu)
         Y = Y + mu*(X-L-S)
